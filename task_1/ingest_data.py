5981e5f258b3fd0d8d0e488953640f6dfc8b9596
Cees-j <Ceesjdu@gmail.com>
2023-05-02 13:58:48 +0100

    rolling median

--- task_1/ingest_data.py
+++ task_1/ingest_data.py
@@ -11,6 +11,7 @@ stock_df = spark.read.csv("task_1/stocks/*.csv", header=True).withColumn("input_
 etf_df = spark.read.csv("task_1/etfs/*.csv", header=True).withColumn("input_file", input_file_name())
 lookup_df = spark.read.csv("task_1/symbols_valid_meta.csv", header=True)  # Assuming it has columns: Symbol and Security Name
 
+
 stock_df = stock_df.withColumnRenamed("Adj Close", "Adj_Close")
 etf_df = etf_df.withColumnRenamed("Adj Close", "Adj_Close")
 lookup_df = lookup_df.withColumnRenamed("Security Name", "Security_Name")
@@ -32,6 +33,7 @@ final_df = combined_df.join(lookup_df, on='Symbol', how='inner')
 final_dataset = final_df.select("Symbol", "Security_Name", "Date", "Open", "High", "Low", "Close", "Adj_Close", "Volume")
 
 
-final_dataset.write.csv("task_1/output/final_dataset.csv", mode="overwrite", header=True)
-final_dataset.write.parquet("task_1/output/final_dataset.parquet", mode="overwrite")
+# final_dataset.write.csv("task_1/output/final_dataset.csv", mode="overwrite", header=True)
+# final_dataset.write.parquet("task_1/output/final_dataset.parquet", mode="overwrite")
 
+print('dtypes', final_dataset.dtypes)
--- task_2/feature_eng.py
+++ task_2/feature_eng.py
@@ -1,10 +1,20 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import Window
-from pyspark.sql.functions import avg, expr, row_number, lag, sum, col
+from pyspark.sql.functions import avg, expr, col, round
+from pyspark.sql.types import FloatType, IntegerType
+
 
 spark = SparkSession.builder.master("local").appName("Moving Average").getOrCreate()
-
 data = spark.read.parquet('task_1/output/final_dataset.parquet')
+
+
+data = data.withColumn("Open", col("Open").cast(FloatType()))
+data = data.withColumn("High", col("High").cast(FloatType()))
+data = data.withColumn("Low", col("Low").cast(FloatType()))
+data = data.withColumn("Close", col("Close").cast(FloatType()))
+data = data.withColumn("Adj_Close", col("Adj_Close").cast(FloatType()))
+data = data.withColumn("Volume", col("Volume").cast(IntegerType()))
+
 
 # Moving Average Window
 moving_average_window = Window.partitionBy('Symbol').orderBy(col('Date').cast('timestamp').cast('long')).rangeBetween(-30 * 86400, 0)
@@ -13,7 +23,18 @@ data.show()
 
 
 # Rolling Median Window
-# rolling_median_window = Window.partitionBy('Symbol').orderBy('Date')
+rolling_median_window = Window.partitionBy('Symbol').orderBy('Date')
+median_percentile = expr('percentile_approx(Adj_Close, 0.5)')
+rolling_med_data = data.withColumn('med_val', median_percentile.over(rolling_median_window))
+rolling_med_data.show()
 
 
-data.show()
+print(data.dtypes)
+for c_name, c_type in data.dtypes:
+    print(c_type)
+    if c_type == 'float':
+        data = data.withColumn(c_name, round(col(c_name),4))
+
+
+data.write.parquet("task_2/output/final_dataset.parquet", mode="overwrite")
+
